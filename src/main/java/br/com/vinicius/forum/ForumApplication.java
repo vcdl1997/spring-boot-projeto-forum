87a18b121fa960a15875034665318329b1d890f4
Vinicius Lima <vcdl1997@gmail.com>
2021-11-29 20:12:54 -0300

    Configuração de Autenticação via Token JWT e configuração do Swagger

--- src/main/java/br/com/vinicius/forum/ForumApplication.java
+++ src/main/java/br/com/vinicius/forum/ForumApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /* Com essa anotação nos habilitamos esse suporte, pro Spring pegar da requisição, 
  * dos parametros da url referentes a paginacao e repassar ao Spring Data
  * */
 @EnableSpringDataWebSupport
 @EnableCaching // Habilita o uso de cache na aplicação.
+@EnableSwagger2
 @SpringBootApplication
 public class ForumApplication {
 
--- src/main/java/br/com/vinicius/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/vinicius/forum/config/security/SecurityConfigurations.java
@@ -1,14 +1,20 @@
 package br.com.vinicius.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.vinicius.forum.repositories.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -16,6 +22,18 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter
 {
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	
 	// Configurações de autenticação (Controle de Acesso, Login e etc...)
 	@Override
@@ -30,13 +48,20 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.anyRequest().authenticated()
-		.and().formLogin();
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(
+			new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), 
+			UsernamePasswordAuthenticationFilter.class
+		);
 	}
 	
 	// Configurações de recursos estáticos (javascript, css e imagens)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		
+		web.ignoring()
+        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 }
